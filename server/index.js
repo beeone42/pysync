b4622524fe54e13720980d424da262629233327b
Sebastien BENOIT <sb@42.fr>
2015-12-01 12:18:55 +0100

    add support for config.json in server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
+var nconf = require('nconf');
 var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
+
 var handle = {};
+nconf.argv().env();
+nconf.file({ file: 'config.json' });
+var conf = nconf.stores.file.store;
+console.log(JSON.stringify(conf));
+
+
+var VERSION = "v1";
 
 handle["/"] = requestHandlers.hello;
-handle["/get_list"] = requestHandlers.get_list;
-handle["/get_file"] = requestHandlers.get_file;
+handle["/api/" + VERSION + "/get_list"] = requestHandlers.get_list;
+handle["/api/" + VERSION +"/get_file"] = requestHandlers.get_file;
 
-server.start(router.route, handle);
+server.start(conf, router.route, handle);
