a7ce983fbb7a24ba553c1935fb3b7a9f36a295d5
Sebastien BENOIT <sb@42.fr>
2015-12-04 14:30:38 +0100

    fix escape in get_list and get_file

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ function get_list(conf, response, s_key) {
 	" LEFT JOIN `keys` ON `keys`.id = `files`.key_id " +
 	" LEFT JOIN `clients` on `clients`.id = `files`.client_id " +
 	" WHERE `clients`.is_master = 1 " +
-	"   AND `keys`.s_key = '" + conf.connection.escape(s_key) + "' " +
+	"   AND `keys`.s_key = " + conf.connection.escape(s_key) +
 	" GROUP BY path ORDER BY mtime DESC";
     console.log(q);
     conf.connection.query(q, function(err, rows, fields) {
@@ -26,8 +26,8 @@ function get_file(conf, response, s_key, path) {
 	" FROM `files` " +
 	" LEFT JOIN `keys` ON `keys`.id = `files`.key_id " +
 	" LEFT JOIN `clients` on `clients`.id = `files`.client_id " +
-	" WHERE `keys`.s_key = '" + conf.connection.escape(s_key) + "' " +
-	"   AND `files`.path = '" + conf.connection.escape(path) + "'";
+	" WHERE `keys`.s_key = " + conf.connection.escape(s_key) +
+	"   AND `files`.path = " + conf.connection.escape(path);
     console.log(q);
     conf.connection.query(q, function(err, rows, fields) {
 	if (!err)
@@ -38,6 +38,7 @@ function get_file(conf, response, s_key, path) {
 	else
 	{
 	    console.log('Error while performing Query.');
+	    console.log(err);
 	    response.fail(err);
 	}
     });
